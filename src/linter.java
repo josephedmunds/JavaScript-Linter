c6004dd691eece86f04da7747eeacbf69bb20f93
josephedmunds <jee431@utulsa.edu>
2015-10-26 14:55:47 -0500

    Implemented spec 2: Trailing whitespace at the end of a line and a newline character at the end of the file

--- src/linter.java
+++ src/linter.java
@@ -5,6 +5,7 @@
  */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class linter {
 
@@ -15,30 +16,65 @@ public class linter {
 
             String line = buff.readLine();
             int lineNum = 0;
-            while(line != null) {
-                if(hasSemicolon(line))
-                    continue;
-                else
+            while (line != null) {
+                if (!hasSemicolon(line))
                     System.out.println(lineNum + ". Statement should end with a semicolon.");
+                if (trailingWhitespace(line))
+                    System.out.println(lineNum + ". Statement should not have trailing whitespace.");
+
                 buff.readLine();
                 lineNum++;
             }
+            if (!endsNewline(buff))
+                System.out.println("File " + file + "should end with a newline character. ");
+
             buff.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Checks if the line ends properly, either with or without a semicolon
+     *
+     * @param line The line to be tested
+     * @return True if the line ends properly
+     */
     public static boolean hasSemicolon(String line) {
         if (line.contains("{")) {
             return true;
-        }
-        else if (line.contains("}")) {
+        } else if (line.contains("}")) {
             return true;
-        }
-        else
+        } else
             return line.endsWith(";");
+    }
+
+    /**
+     * Checks if there is trailing whitespace on a line of code
+     *
+     * @param line The line to be tested
+     * @return True if there is trailing whitespace on the line
+     */
+    public static boolean trailingWhitespace(String line) {
+        if (line.matches(".*/s*"))
+            return true;
+        else return false;
+    }
+
+    /**
+     * Checks if the file ends in an newline
+     *
+     * @param file The file being linted
+     * @return True if the file properly ends in a newline
+     */
+    public static boolean endsNewline(BufferedReader file) {
+        Scanner scanMan = new Scanner(file);
+        String currLine = scanMan.nextLine();
+        while (scanMan.hasNextLine()) {
+            currLine = scanMan.nextLine();
+        }
+        if (currLine.matches(".*\n"))
+            return true;
+        else return false;
     }
 }
