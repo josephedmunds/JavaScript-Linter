9e457a68b8570d0309d504a04cc2cbc422b9bae5
josephedmunds <jee431@utulsa.edu>
2015-12-19 15:26:52 -0600

    Test file added to repository

---
+++ test.js
@@ -0,0 +1,24 @@
+var x;
+var y    
+
+var z = functionCall () { }
+
+x = y + z  
+
+def itsAFunction (sendThingy) {
+    doThing()
+}
+
+def theOtherFunctionThing (otherSendThingy)
+{
+    x == doThing2();
+    y === doThing2();
+}
+
+"quote1";
+'quote2'
+"quote'3";
+'quote'4';
+
+dothethingsthatislongerthan80characterssothathelinterbreaksyaygoteamdoitjustdoitshialabeouf;
+var x; var y;
